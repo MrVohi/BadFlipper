a20deb0dd8158a4288e69a624cac111f418864a0
Mr Vohi <89097739+MrVohi@users.noreply.github.com>
2025-01-01 21:49:35 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -43,8 +43,7 @@ $Body = [PSCustomObject]@{
 
 }
 
-if (-not ([string]::IsNullOrEmpty($text))){Invoke-restmethod -Uri $DiscordUrl -Method Post -Body ($Body | ConvertTo-Json) -Headers @{ "Content-Type" = "application/json" }};
-# Invoke-RestMethod -ContentType 'Application/Json' -Uri $DiscordUrl  -Method Post -Body ($Body | ConvertTo-Json -Depth 4)};
+if (-not ([string]::IsNullOrEmpty($text))){Invoke-RestMethod -ContentType 'Application/Json' -Uri $DiscordUrl  -Method Post -Body ($Body | ConvertTo-Json -Depth 4)};
 
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $DiscordUrl}
 }
